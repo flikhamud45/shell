d88a6aa7626a12c75e7cb5c4c1e6945fa1861ed3
Ofek Lavi <flikhamud123@gmail.com>
2025-06-30 19:47:09 +0300

    solve

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import abc
-from typing import List, Optional, Tuple
+from typing import Callable, List, Optional, Tuple
 import time
 import os
 
 EXIT = "exit"
+commands_hostory: List[str] = []
+MAX_HISTORY_SIZE = 100
+env_variables: dict[str, str] = {}
+aliases: dict[str, str] = {}
+
+
+def add_to_history(command: str) -> None:
+    """Add a command to the history, maintaining a maximum size."""
+    commands_hostory.append(command)
+    if len(commands_hostory) > MAX_HISTORY_SIZE:
+        commands_hostory.pop(0)  # Remove the oldest command if history exceeds max size
 
 
 class Command(abc.ABC):
@@ -25,7 +36,12 @@ class Echo(Command):
 
     @staticmethod
     def run(*args) -> None:
-        print(" ".join(args))
+        for arg in args:
+            if arg.startswith("$"):
+                var_name = arg[1:]
+                value = env_variables.get(var_name, "")
+            print(value, end=" ")
+        print()
 
 
 class Man(Command):
@@ -140,6 +156,353 @@ class sleep(Command):
             print("Invalid number of seconds.")
 
 
+class pwd(Command):
+    name: str = "pwd"
+    doc: str = "Print the current working directory."
+    usage: str = ""
+
+    @staticmethod
+    def run() -> None:
+        print(os.getcwd())
+
+
+def check_path_exist(pos: int = 0, name: str = "path") -> Callable:
+    """Decorator factory to check if a file or directory exists before running the command."""
+
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            path = args[pos] if args else kwargs[name]
+            if not os.path.exists(path):
+                print(f"No such file or directory: {path}")
+                return
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def check_is_directory(pos: int = 0, name: str = "path") -> Callable:
+    """Decorator factory to check if a path is a directory before running the command."""
+
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            path = args[pos] if args else kwargs[name]
+            if not os.path.isdir(path):
+                print(f"Not a directory: {path}")
+                return
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def check_is_file(pos: int = 0, name: str = "path") -> Callable:
+    """Decorator factory to check if a path is a file before running the command."""
+
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            path = args[pos] if args else kwargs[name]
+            if not os.path.isfile(path):
+                print(f"Not a file: {path}")
+                return
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+class cd(Command):
+    name: str = "cd"
+    doc: str = "Change the current working directory."
+    usage: str = "<directory>"
+
+    @staticmethod
+    @check_path_exist()
+    @check_is_directory()
+    def run(path: str) -> None:
+        os.chdir(path)
+
+
+class ls(Command):
+    name: str = "ls"
+    doc: str = "List files in the current directory."
+    usage: str = "[directory]"
+
+    @staticmethod
+    @check_path_exist()
+    @check_is_directory()
+    def run(path: str = ".") -> None:
+        files = os.listdir(path)
+        print(" ".join(files))
+
+
+class mkdir(Command):
+    name: str = "mkdir"
+    doc: str = "Create a new directory."
+    usage: str = "<directory_name>"
+
+    @staticmethod
+    def run(path: str) -> None:
+        if os.path.exists(path):
+            print(f"Directory already exists: {path}")
+            return
+        os.mkdir(path)
+
+
+class rmdir(Command):
+    name: str = "rmdir"
+    doc: str = "Remove an empty directory."
+    usage: str = "<directory_name>"
+
+    @staticmethod
+    @check_path_exist()
+    @check_is_directory()
+    def run(path: str) -> None:
+        os.rmdir(path)
+
+
+class touch(Command):
+    name: str = "touch"
+    doc: str = "Create a new file or update the timestamp of an existing file."
+    usage: str = "<file_name>"
+
+    @staticmethod
+    def run(path: str) -> None:
+        if os.path.exists(path):
+            print(f"File already exists: {path}")
+        else:
+            with open(path, "w"):
+                pass
+
+
+class rm(Command):
+    name: str = "rm"
+    doc: str = "Remove a file."
+    usage: str = "<file_name>"
+
+    @staticmethod
+    @check_path_exist()
+    @check_is_file()
+    def run(path: str) -> None:
+        delete_confirmation = input(
+            f"Are you sure you want to delete '{path}'? (y/[n]): "
+        )
+        if delete_confirmation.lower() == "y":
+            os.remove(path)
+            print(f"File '{path}' deleted.")
+        else:
+            print("Deletion cancelled.")
+
+
+class cat(Command):
+    name: str = "cat"
+    doc: str = "Display the contents of a file."
+    usage: str = "<file_name>"
+
+    @staticmethod
+    def run(*file_names: str) -> None:
+        for file_name in file_names:
+            if not os.path.exists(file_name):
+                print(f"No such file: {file_name}")
+                continue
+            if not os.path.isfile(file_name):
+                print(f"{file_name} is not a file.")
+                continue
+            try:
+                with open(file_name, "r") as f:
+                    print(f.read())
+            except PermissionError:
+                print(f"Permission denied: {file_name}")
+
+
+class head(Command):
+    name: str = "head"
+    doc: str = "Display the first few lines of a file."
+    usage: str = "<number_of_lines> <file_name>"
+
+    @staticmethod
+    @check_path_exist(pos=1, name="file_name")
+    @check_is_file(pos=1, name="file_name")
+    def run(number_of_lines: str, file_name: str) -> None:
+        try:
+            num_lines = int(number_of_lines)
+        except ValueError:
+            print("Invalid number of lines.")
+            return
+        if num_lines < 0:
+            print("Number of lines must be non-negative.")
+            return
+        with open(file_name, "r") as f:
+            for _ in range(num_lines):
+                line = f.readline()
+                if not line:
+                    break
+                print(line, end="")
+
+
+class tail(Command):
+    name: str = "tail"
+    doc: str = "Display the last few lines of a file."
+    usage: str = "<number_of_lines> <file_name>"
+
+    @staticmethod
+    @check_is_file(pos=1, name="file_name")
+    @check_path_exist(pos=1, name="file_name")
+    def run(number_of_lines: str, file_name: str) -> None:
+        try:
+            num_lines = int(number_of_lines)
+        except ValueError:
+            print("Invalid number of lines.")
+            return
+        if num_lines < 0:
+            print("Number of lines must be non-negative.")
+            return
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            for line in lines[-num_lines:]:
+                print(line, end="")
+
+
+class write(Command):
+    name: str = "write"
+    doc: str = "Write text to a file."
+    usage: str = "<file_name> [text...]"
+
+    @staticmethod
+    def run(file_name: str, *text: str) -> None:
+        if os.path.exists(file_name) and not os.path.isfile(file_name):
+            print(f"{file_name} is not a file.")
+            return
+        with open(file_name, "a") as f:
+            f.write(" ".join(text) + "\n")
+        print(f"Text written to {file_name}")
+
+
+def remove_quates(func: Callable) -> Callable:
+    def wrapper(*args: str) -> None:
+        new_args = []
+        curr_arg = ""
+        in_quotes = False
+        for arg in args:
+            if arg.startswith('"') and not in_quotes:
+                in_quotes = True
+                curr_arg = arg[1:]
+            elif arg.endswith('"') and in_quotes:
+                in_quotes = False
+                curr_arg += " " + arg[:-1]
+                new_args.append(curr_arg)
+                curr_arg = ""
+            elif in_quotes:
+                curr_arg += " " + arg
+            else:
+                new_args.append(arg)
+        if curr_arg:
+            print("Unmatched quotes in arguments.")
+            return
+        return func(*new_args)
+
+
+class grep(Command):
+    name: str = "grep"
+    doc: str = "Search for a pattern in a file."
+    usage: str = "<pattern> <file_name>"
+
+    @staticmethod
+    @remove_quates
+    @check_path_exist(pos=1, name="file_name")
+    @check_is_file(pos=1, name="file_name")
+    def run(pattern: str, file_name: str) -> None:
+        with open(file_name, "r") as f:
+            for line in f:
+                if pattern in line:
+                    print(line, end="")
+
+
+class history(Command):
+    name: str = "history"
+    doc: str = "Display the commands history."
+    usage: str = ""
+
+    @staticmethod
+    def run() -> None:
+        if not commands_hostory:
+            print("No commands in history.")
+            return
+        for i, command in enumerate(commands_hostory, start=1):
+            print(f"{i}: {command}")
+
+
+class set(Command):
+    name: str = "set"
+    doc: str = "Set an environment variable."
+    usage: str = "<variable_name> <value>"
+
+    @staticmethod
+    @remove_quates
+    def run(variable_name: str, value: str) -> None:
+        if not variable_name.isidentifier():
+            print(f"Invalid variable name: {variable_name}")
+            return
+        env_variables[variable_name] = value
+
+
+class unset(Command):
+    name: str = "unset"
+    doc: str = "Unset an environment variable."
+    usage: str = "<variable_name>"
+
+    @staticmethod
+    def run(variable_name: str) -> None:
+        if variable_name in env_variables:
+            del env_variables[variable_name]
+        else:
+            print(f"Variable '{variable_name}' not set.")
+
+
+class env(Command):
+    name: str = "env"
+    doc: str = "Display all environment variables."
+    usage: str = ""
+
+    @staticmethod
+    def run() -> None:
+        if not env_variables:
+            print("No environment variables set.")
+            return
+        for var, value in env_variables.items():
+            print(f"{var}: {value}")
+
+
+class alias(Command):
+    name: str = "alias"
+    doc: str = "Create an alias for a command."
+    usage: str = "<alias_name> <command>"
+
+    @staticmethod
+    @remove_quates
+    def run(alias_name: str, command: str) -> None:
+        if not alias_name.isidentifier():
+            print(f"Invalid alias name: {alias_name}")
+            return
+        aliases[alias_name] = command
+
+
+class unalias(Command):
+    name: str = "unalias"
+    doc: str = "Remove an alias."
+    usage: str = "<alias_name>"
+
+    @staticmethod
+    def run(alias_name: str) -> None:
+        if alias_name in aliases:
+            del aliases[alias_name]
+        else:
+            print(f"Alias '{alias_name}' not found.")
+
+
 COMMANDS = {c.name: c for c in Command.__subclasses__()}
 
 
@@ -149,13 +512,19 @@ def parse_command(line: str) -> Tuple[str, List[str]]:
     if not line:
         return "", []
     parts = line.split()
-    return parts[0], parts[1:]
+    command_name = parts[0]
+    args = parts[1:]
+    if command_name in aliases:
+        command_name = aliases[command_name]
+        return parse_command(command_name + " " + " ".join(args))
+    return command_name, args
 
 
 def run_shell() -> None:
     try:
         while True:
             line = input(">>> ")
+            add_to_history(line)
             command_name, args = parse_command(line)
             if command_name == EXIT:
                 break
